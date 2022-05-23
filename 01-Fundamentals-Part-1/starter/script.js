0c486ca7462f344d5588c8261ad8db3e6c725b9f
KarthickSubburaj5200 <karthick.mycareer@hotmail.com>
2022-05-23 11:10:16 +0530

    learned values and variables learned dynamic variables learned comments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,105 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is Fun!');
 
-40+8+23-10;
-console.log(40+8+23-10);
+//values and variables
+
+//let js = 'amazing';
+//if (js === 'amazing') alert('JavaScript is Fun!');
+
+//40+8+23-10;
+//console.log(40+8+23-10);
+
+//console.log('Jonas');
+//console.log(23);
+
+//let firstName = "Matilda"
+//let firstnamePerson
+//let First_Name_Person
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+
+//let 3years = 3;  Uncaught SystaxError: Invalid or unexpected token
+
+// let jonas&Matilda = "JM"; // error
+
+// let new = "JM"  error
+
+//let PI = 3.1415;
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+//
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+
+// console.log(myFirstJob);
+
+
+// OBJECTS AND PRIMITIVE
+
+//VALUE => OBJECT OR PRIMITIVE
+
+
+{/*
+  Object
+
+  1. let me = {
+        name: 'Jonas'
+  }
+*/}
+
+{/*
+  Primitive
+
+ let firstname = 'Jonas';
+ let age = 30;
+*/}
+
+// There are 7 Primitive Data Types
+
+// 1. Numbers: Floating point numbers => used for decimals and integers let age = 23;
+// 2. String: Sequence of characters => used for text let firstName = 'Jonas';
+// 3. Boolean: Logical type that can only be true or false => Used for taking decision let fullAge = true;
+// 4. Undefined: Value taken by a variable that is not yet defined ('empty value')
+// 5. Null: Also means 'empty value'
+// 6.Symbol (ES2015): Value that is unique and cannot be changed [not useful for now]
+// 7. BigInt(ES2020): Larger integers than the number type can hold
+
+
+
+
+//comments :
+//Variable name convertions
+//true;
+//console.log(true)
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+
+//Dynamic typing
+
+
+javascriptIsFun = 'Yes!';
+console.log(javascriptIsFun)
+console.log( typeof javascriptIsFun)
+
+
+let year;
+console.log(year);
+console.log( typeof year);
+
+year = 1991;
+
+console.log(year);
+console.log( typeof year);
+
+
+console.log(typeof null);
